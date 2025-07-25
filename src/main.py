6d003093db710424a638c10f3fc0caaef63c4e1f
Olga Zhivaeva <olga.zhiv@yandex.ru>
2025-07-25 20:50:06 +0300

    Add Pydantic model

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel, ConfigDict, Field
 
 FRONTEND_DIR = Path(__file__).parent / "frontend"
 STATIC_FILES_DIR = Path(__file__).parent / "static"
@@ -9,21 +10,44 @@ STATIC_FILES_DIR = Path(__file__).parent / "static"
 app = FastAPI()
 
 
+class UserProfile(BaseModel):
+    profile_id: int = Field(..., description="Уникальный идентификатор профиля пользователя")
+    email: str = Field(..., description="Электронный адрес пользователя для связи и уведомлений")
+    username: str = Field(..., description="Имя пользователя для входа в систему и идентификации")
+    registered_at: str = Field(..., description="Дата регистрации пользователя в системе")
+    updated_at: str = Field(..., description="Дата последнего обновления профиля пользователя")
+    is_active: bool = Field(..., description="Флаг, указывающий, активен ли профиль пользователя")
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "profile_id": 123,
+                "email": "test@example.com",
+                "username": "testuser",
+                "registered_at": "2025-01-01",
+                "updated_at": "2025-01-01",
+                "is_active": True,
+            },
+        },
+    )
+
+
 @app.get(
     "/frontend-api/users/me",
     summary="Получить данные пользователя",
     response_description="Данные пользователя",
     tags=["Users"],
+    response_model=UserProfile,
 )
-def mock_get_user():
-    mock_user_data = {
-        "profile_id": 1,
-        "email": "IIvanov@example.com",
-        "username": "Иван",
-        "registered_at": "2025-07-01",
-        "updated_at": "2025-07-01",
-        "is_active": True,
-        }
+def mock_get_user() -> UserProfile:
+    mock_user_data = UserProfile(
+        profile_id=1,
+        email="IIvanov@example.com",
+        username="Иван",
+        registered_at="2025-07-01",
+        updated_at="2025-07-01",
+        is_active=True,
+    )
     return mock_user_data
 
 
