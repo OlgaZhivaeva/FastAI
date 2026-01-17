40480dfcef0c928efc065e697be5bb2001d4ed58
Olga Zhivaeva <olga.zhiv@yandex.ru>
2026-01-17 18:02:55 +0300

    Add FastAPI decorators to router.py

--- src/sites/router.py
+++ src/sites/router.py
@@ -1,40 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.responses import HTMLResponse
 
-from src.sites.create_site import mock_create_site
-from src.sites.generate_html import generate_html_stream
+from src.sites.create_site import CreateSiteRequest, mock_create_site
+from src.sites.generate_html import SiteGenerationRequest, generate_html_stream
 from src.sites.get_site import SiteResponse, mock_get_site
 from src.sites.get_user_sites import GeneratedSitesResponse, mock_get_user_sites
 
 sites_router = APIRouter(prefix="/frontend-api/sites", tags=["Sites"])
 
-sites_router.get(
+
+@sites_router.get(
     "/my",
     summary="Получить список сайтов пользователя",
     response_description="Сайты пользователя",
     response_model=GeneratedSitesResponse,
-)(mock_get_user_sites)
+)
+async def _get_user_sites(http_request: Request):
+    return mock_get_user_sites(http_request)
 
 
-sites_router.get(
+@sites_router.get(
     "/{site_id}",
     summary="Получить сайт",
     response_description="Данные сайта",
     response_model=SiteResponse,
-)(mock_get_site)
+)
+async def _get_site(site_id: int, http_request: Request):
+    return mock_get_site(site_id, http_request)
 
 
-sites_router.post(
+@sites_router.post(
     "/create",
     summary="Создать сайт",
     response_description="Данные для генерации сайта",
     response_model=SiteResponse,
-)(mock_create_site)
+)
+async def _create_site(request: CreateSiteRequest):
+    return mock_create_site(request)
 
 
-sites_router.post(
+@sites_router.post(
     "/{site_id}/generate",
     summary="Сгенерировать HTML код сайта",
     response_description="HTML код сайта",
     response_class=HTMLResponse,
-)(generate_html_stream)
+)
+async def _generate_html(site_id: int, request: SiteGenerationRequest, http_request: Request):
+    return await generate_html_stream(site_id, request, http_request)
--- src/users/router.py
+++ src/users/router.py
@@ -4,9 +4,12 @@ from src.users.get_user import UserDetailsResponse, mock_get_user
 
 users_router = APIRouter(prefix="/frontend-api/users", tags=["Users"])
 
-users_router.get(
+
+@users_router.get(
     "/me",
     summary="Получить данные пользователя",
     response_description="Данные пользователя",
     response_model=UserDetailsResponse,
-)(mock_get_user)
+)
+async def _get_user():
+    return mock_get_user()
